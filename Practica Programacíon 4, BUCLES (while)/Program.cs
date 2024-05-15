8f641b4af0fc1b1de1fb388dfac70196fcb01b47
marcosz-96 <168491483+marcosz-96@users.noreply.github.com>
2024-05-14 22:07:13 -0300

    Agregar archivos de proyecto.

---
+++ Practica Programacíon 4, BUCLES (while)/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Programacíon_4__BUCLES__while_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1 de la Guía 4: Números correlativos desde el número ingresado
+
+            /*int numIncial = 0;
+            int cont = 0;
+
+            Console.Write("Ingrese un número positivo: ");
+            numIncial = int.Parse(Console.ReadLine());
+
+            while (numIncial > 0 && cont <= numIncial ) 
+            {
+                Console.WriteLine(" Los siguientes números son: " + cont);
+                cont++;
+            }*/
+            
+                      
+            //Ejercicio 2 de la Guía 4: Carga, Suma y Promedio de valores
+
+            /*int cont1 = 1;
+            int numIngresado = 0;
+            int suma = 0;
+            int promedio = 0; 
+
+            while (cont1 <= 10)
+            {
+                Console.WriteLine("Ingrese un número");
+                numIngresado = int.Parse(Console.ReadLine());
+                suma = suma + numIngresado;
+                promedio = suma / 10;
+
+                cont1++;
+            }
+            Console.WriteLine("La suma de los valores es igual a: " + suma);
+            Console.WriteLine("El promedio es igual a: " + promedio);
+            Console.WriteLine("Fin de la operación!");*/
+        }
+
+    }
+}
